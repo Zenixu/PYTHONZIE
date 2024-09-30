6dc6137961e453fe089118bc1b20671f63718047
Zennrch <165318106+Zenixu@users.noreply.github.com>
2024-09-30 20:03:57 +0700

    Delete Runtutan/Latihan Dasar Algoritma directory

--- Runtutan/Latihan Dasar Algoritma/Lda6.py
+++
@@ -1,7 +0,0 @@
-nama = input("Siapa nama kamu: ")
-nilai = int(input("Masukkan nilai kamu: "))
-
-if nilai > 80:
-    print(f"Selamat {nama}, Kamu telah Lulus dengna nilai {nilai}")
-else:
-    print(f"Yahhh {nama}, Kamu Tidak Lulus dengan nilai {nilai} di bawah KKM SI BRO\n")
