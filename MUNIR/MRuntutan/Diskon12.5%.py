99e01787e39e401367bb9342eefc69d38b14c9c9
= <=>
2024-09-30 20:24:53 +0700

    SOAL SOAL MUNIR

---
+++ MUNIR/MRuntutan/Diskon12.5%.py
@@ -0,0 +1,8 @@
+print("====================================================")
+hb = int(input('berapa total harga barang yang konsumen beli: Rp.'))
+d = 12.5
+
+total = ((100-d)/100*hb)
+
+print("====================================================")
+print (f"jadi total harga barang yang konsumen beli setelah diskon adalah = Rp.{total}  \n")
