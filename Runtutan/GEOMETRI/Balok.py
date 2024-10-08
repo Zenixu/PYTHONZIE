d9bccb935dfa3b8cb8911532bda1334aeb3707f4
= <=>
2024-10-08 08:33:35 +0700

    runtutan

--- Runtutan/GEOMETRI/Balok.py
+++ Runtutan/GEOMETRI/Balok.py
@@ -7,11 +7,11 @@ l = int(input("masukkan lebarnya: "))
 t = int(input("masukkan tingginya: "))
 
 v = p * l * t
-l = 2 * (p * l + p * t + l * t)
+ls = 2 * (p * l + p * t + l * t)
 k = 4 * (p + l + t)
 
 print(f'''
 VOLUME BALOK: {v}
-LUAS BALOK: {l}
+LUAS BALOK: {ls}
 KELILING BALOK: {k}
 ''')
