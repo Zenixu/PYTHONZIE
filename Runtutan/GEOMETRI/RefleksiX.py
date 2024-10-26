99281409b56b2724780b5470e6e1de403ea1d4ef
= <=>
2024-10-26 19:56:50 +0700

    Runtutan

---
+++ Runtutan/GEOMETRI/RefleksiX.py
@@ -0,0 +1,12 @@
+print('\n')
+print('='*30)
+print('Refleksi terhadap sumbu x')
+print('='*30)
+print()
+
+x = int(input('Masukan sumbu x : '))
+y = int(input('Masukan sumbu y : '))
+
+A = y * -1
+
+print(f'Hasil refleksi terhadap sumbu x dari titk ({x}, {y}) adalah ({x}, {A})')
